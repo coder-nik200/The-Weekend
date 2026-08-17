585c7395784d17e03cd2ee1fa2d9e3af7d5208b2
coder-nik200 <nitishkb1707@gmail.com>
2026-08-17 15:38:02 +0530

    Add Google A4

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,30 @@
-import './globals.css';
+import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
-  title: 'The Weeknd — After Hours',
-  description: 'A midnight broadcast.'
+  title: "The Weeknd — After Hours",
+  description: "A midnight broadcast.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-B3CS6DNKQP"
+        />
+
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-B3CS6DNKQP');
+          `}
+        </Script>
+      </head>
+
       <body>{children}</body>
     </html>
   );
